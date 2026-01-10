c9a40d93ef284ed88301897a73b7be598585d78d
subhanalitahirdev-cloud <subhanalitahir.dev@gmail.com>
2026-01-10 23:11:22 +0500

    Learn basic of nodejs and theory of nodejs

---
+++ Day_1/script.js
@@ -0,0 +1,33 @@
+const file = require("fs");
+// creating file using Node.js
+// file.writeFile(
+//   "day.txt",
+//   "This is my first file created using Node.js",
+//   function (error) {
+//     if (error) {
+//       console.log("Error occured", error);
+//     } else {
+//       console.log("done");
+//     }
+//   }
+// );
+// Editing file using Node.js
+// file.appendFile(
+//   "day.txt",
+//   "\nNode.js was created in 2009 by Ryan Dahl. It is used to create I/O intensive web applications like video streaming sites, single page applications, and other web applications.",
+//   function (error) {
+//     if (error) {
+//       console.log("Error is occured", error);
+//     } else {
+//       console.log("File is Edited");
+//     }
+//   }
+// );
+// Removing file using Node.js
+// file.unlink("day.txt", function (error) {
+//   if (error) {
+//     console.log("Error is occured", error);
+//   } else {
+//     console.log("FIle is deleted");
+//   }
+// });
